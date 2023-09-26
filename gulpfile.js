534e6ba73e435d58e866981bf3ff55954f353a53
Robotix703 <stephane.chatelle@orange.fr>
2023-09-26 19:49:58 +0000

    Update gulpfile to zip all files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 
 exports.default = () => (
-	gulp.src(['*', '!build', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
+	gulp.src(['**/*', '!build', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
 		.pipe(zip('stargatecoalition.zip'))
 		.pipe(gulp.dest('build'))
 );
