14b89c9ae157757bcf23d91ea694c82b93c449a1
Robotix703 <stephane.chatelle@orange.fr>
2023-09-27 17:29:18 +0000

    Use https://gitlab.com/foundrynet/worldbuilding

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const less = require('gulp-less');
 
-exports.default = () => (
-	gulp.src(['**/*', '!build', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
+function createZip() {
+	return gulp.src(['**/*', '!build', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
 		.pipe(zip('stargatecoalition.zip'))
 		.pipe(gulp.dest('build'))
+}
+
+/* ----------------------------------------- */
+/*  Compile LESS
+/* ----------------------------------------- */
+
+function compileLESS() {
+  	return gulp.src("styles/stargatecoalition.less")
+		.pipe(less())
+		.pipe(gulp.dest("./styles/"))
+}
+
+/* ----------------------------------------- */
+/*  Export Tasks
+/* ----------------------------------------- */
+
+exports.default = gulp.series(
+	compileLESS,
+  	createZip
 );
