34e19afd0335b5c867216743ed0e204915145f26
Robotix703 <stephane.chatelle@orange.fr>
2023-10-03 19:05:23 +0000

    Add delete to clean build folder for debug

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const less = require('gulp-less');
+const del = require('del');
+
+function clean() {
+	return del('build/**', {force:true});
+}
 
 function copyForZip() {
 	return gulp.src(['**/*', '!build/*', '!build', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
 		.pipe(gulp.dest('build'))
 }
-
-/* ----------------------------------------- */
-/*  Compile LESS
-/* ----------------------------------------- */
 
 function compileLESS() {
   	return gulp.src("styles/stargatecoalition.less")
@@ -22,6 +23,7 @@ function compileLESS() {
 /* ----------------------------------------- */
 
 exports.default = gulp.series(
+	clean,
 	compileLESS,
 	copyForZip
 );
