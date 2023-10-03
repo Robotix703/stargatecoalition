a7e118d790b72c1205d53fe147a6d601811744a8
Robotix703 <stephane.chatelle@orange.fr>
2023-10-03 19:46:38 +0000

    Try fix css

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -12,10 +12,8 @@ export class SimpleActorSheet extends ActorSheet {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["stargatecoalition", "sheet", "actor"],
       template: "systems/stargatecoalition/templates/actor-sheet.html",
-      width: 600,
-      height: 600,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
-      scrollY: [".biography", ".items", ".attributes"],
+      scrollY: [".biographie", ".items", ".attributes"],
       dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
     });
   }
